8457abeaeabe59ac8cac1e7d64beeee3842dee63
Esther <est.eacanalesc0201@uml.edu.ni>
2025-06-13 19:27:44 -0600

    listo

--- ejercicios-js/ejercicio2.js
+++ ejercicios-js/ejercicio2.js
@@ -1,11 +1,14 @@
-const secreto = Math.floor(Math.random() * 100) + 1;
+// Juego de adivinar el número
 let intento;
+const secreto = Math.floor(Math.random() * 100) + 1; // Generar el número secreto solo una vez
+
 do {
-  intento = Math.floor(Math.random() * 100) + 1;
+  intento = Math.floor(Math.random() * 100) + 1; // Simula un intento aleatorio
   if (intento < secreto) {
     console.log(`${intento} es menor ❌`);
   } else if (intento > secreto) {
     console.log(`${intento} es mayor ❌`);
   }
 } while (intento !== secreto);
+
 console.log(`¡Correcto! El número era ${secreto} ✅`);
